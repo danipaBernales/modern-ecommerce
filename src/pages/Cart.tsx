3194eb1d57e41455949733dc53e181fa5c90b9d4
danipaBernales <dbernales.miranda@gmail.com>
2025-03-01 13:06:18 -0300

    Update Cart & Categories

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -195,7 +195,7 @@ export default function Cart() {
               to="/products"
               className="w-full flex items-center justify-center bg-gray-100 dark:bg-gray-700 text-gray-800 dark:text-white px-6 py-3 rounded-lg hover:bg-gray-200 dark:hover:bg-gray-600 transition-colors text-center btn-hover-shadow mb-4"
             >
-              Add More Products
+              Add more products
             </Link>
             <button
               onClick={checkout}
