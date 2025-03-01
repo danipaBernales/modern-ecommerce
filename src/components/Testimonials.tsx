5225edf243a496d85584f77813958e24211a8748
danipaBernales <dbernales.miranda@gmail.com>
2025-03-01 12:49:31 -0300

    Update Testimonials

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -32,7 +32,7 @@ export default function Testimonials() {
     <section className="py-12 mt-16">
       <div className="container mx-auto px-4">
         <h2 className="text-3xl font-bold text-gray-900 dark:text-white mb-8 text-center">
-          What Our Customers Say
+          What our customers say
         </h2>
         
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
